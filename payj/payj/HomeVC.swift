3fbb449bce181378242157ad4f29dbf896f610d8
Saurav Mitra <mitrasaurav97@gmail.com>
2018-02-06 18:40:27 +0000

    Add basic buttons to HomeVC

--- payj/payj/HomeVC.swift
+++ payj/payj/HomeVC.swift
@@ -9,15 +9,54 @@
 import UIKit
 
 class HomeVC: UIViewController {
-  let balanceView: UIView!
-  let sendButton: UIButton!
-  let settingsButton: UIButton!
-  let cameraButton: UIButton!
+  var balanceView: UIView!
+  var sendButton: UIButton!
+  var settingsButton: UIButton!
+  var cameraButton: UIButton!
 
   override func viewDidLoad() {
     self.viewDidLoad()
 
-    
+    balanceView = UIView()
+    let balanceLabel = UILabel()
+    balanceLabel.text = "$2.54"
+    balanceView.addSubview(balanceLabel)
+    NSLayoutConstraint.activate([
+      balanceLabel.leftAnchor.constraint(equalTo: balanceView.leftAnchor),
+      balanceLabel.rightAnchor.constraint(equalTo: balanceView.rightAnchor),
+      balanceLabel.topAnchor.constraint(equalTo: balanceView.topAnchor),
+      balanceLabel.bottomAnchor.constraint(equalTo: balanceView.bottomAnchor)
+      ])
+    view.addSubview(balanceView)
+    NSLayoutConstraint.activate([
+      balanceView.leftAnchor.constraint(equalTo: view.leftAnchor),
+      balanceView.rightAnchor.constraint(equalTo: view.rightAnchor),
+      balanceView.topAnchor.constraint(equalTo: view.topAnchor),
+      balanceView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+      ])
 
+    sendButton = UIButton()
+    sendButton.setTitle("Send", for: .normal)
+    view.addSubview(balanceView)
+    NSLayoutConstraint.activate([
+      sendButton.rightAnchor.constraint(equalTo: view.rightAnchor),
+      sendButton.topAnchor.constraint(equalTo: view.topAnchor)
+      ])
+
+    settingsButton = UIButton()
+    settingsButton.setTitle("Settings", for: .normal)
+    view.addSubview(balanceView)
+    NSLayoutConstraint.activate([
+      balanceView.leftAnchor.constraint(equalTo: view.leftAnchor),
+      balanceView.topAnchor.constraint(equalTo: view.topAnchor)
+      ])
+
+    cameraButton = UIButton()
+    cameraButton.setTitle("Camera", for: .normal)
+    view.addSubview(balanceView)
+    NSLayoutConstraint.activate([
+      balanceView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      balanceView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+      ])
   }
 }

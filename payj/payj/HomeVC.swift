d25d9a7c887b0a6e2ac30a802511ca8eff7d18d4
Saurav Mitra <mitrasaurav97@gmail.com>
2018-02-06 18:28:03 +0000

    Initial iOS proj commit

---
+++ payj/payj/HomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVC.swift
+//  payj
+//
+//  Created by Saurav Mitra on 06/02/2018.
+//  Copyright © 2018 PayJ. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+  let balanceView: UIView!
+  let sendButton: UIButton!
+  let settingsButton: UIButton!
+  let cameraButton: UIButton!
+
+  override func viewDidLoad() {
+    self.viewDidLoad()
+
+    
+
+  }
+}

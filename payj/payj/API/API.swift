1595c5e72e8597261b195fde26d0f90d3b93eb91
Saurav Mitra <mitrasaurav97@gmail.com>
2018-02-14 23:33:45 +0000

    Update skeleton API

---
+++ payj/payj/API/API.swift
@@ -0,0 +1,29 @@
+//
+//  API.swift
+//  payj
+//
+//  Created by Saurav Mitra on 13/02/2018.
+//  Copyright © 2018 PayJ. All rights reserved.
+//
+
+struct API {
+  static func pay(user: User, completion: @escaping () -> Void) {
+
+  }
+
+  static func getTransactions(completion: @escaping () -> Void) {
+
+  }
+
+  static func trust(contact: User, completion: @escaping () -> Void) {
+
+  }
+
+  static func untrust(contact: User, completion: @escaping () -> Void) {
+
+  }
+
+  static func getBalance(completion: @escaping (Float) -> Void) {
+
+  }
+}
---
+++ payj/payj/API/User.swift
@@ -0,0 +1,29 @@
+//
+//  user.swift
+//  payj
+//
+//  Created by Saurav Mitra on 14/02/2018.
+//  Copyright © 2018 PayJ. All rights reserved.
+//
+
+extension API {
+  static func register(completion: @escaping (User) -> Void) {
+    
+  }
+  
+  static func getMe(completion: @escaping (User) -> Void) {
+    
+  }
+  
+  static func login(completion: @escaping () -> Void) {
+    
+  }
+  
+  static func verify(completion: @escaping () -> Void) {
+    
+  }
+  
+  static func completeRegistration(withAlias alias: String, completion: @escaping () -> Void) {
+    
+  }
+}

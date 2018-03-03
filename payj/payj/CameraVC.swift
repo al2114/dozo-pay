47b76d163f00f0277e3ea598f0204a1aef846203
Saurav Mitra <mitrasaurav97@gmail.com>
2018-03-03 18:10:34 +0000

    Refactor QR reader

--- payj/payj/CameraVC.swift
+++ payj/payj/CameraVC.swift
@@ -10,78 +10,32 @@ import UIKit
 import AVFoundation
 
 class CameraVC: UIViewController {
-  var label: UILabel!
+  var qrReader: QRReader!
 
-  var captureSession: AVCaptureSession!
-  var videoPreviewLayer: AVCaptureVideoPreviewLayer!
-  var qrCodeFrameView: UIView!
+  var captureReceiptButton: UIButton!
 
   override func viewDidLoad() {
     // Get the back-facing camera for capturing videos
-    let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back)
-    
-    guard let captureDevice = deviceDiscoverySession.devices.first else {
-      print("Failed to get the camera device")
-      return
-    }
-    
-    do {
-      // Get an instance of the AVCaptureDeviceInput class using the previous device object.
-      let input = try AVCaptureDeviceInput(device: captureDevice)
-      
-      // Set the input device on the capture session.
-      captureSession = AVCaptureSession()
-      captureSession.addInput(input)
+    view.backgroundColor = .primaryBackground
 
-      // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
-      let captureMetadataOutput = AVCaptureMetadataOutput()
-      captureSession.addOutput(captureMetadataOutput)
-      captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-      captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-
-      qrCodeFrameView = UIView()
-      qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
-      qrCodeFrameView.layer.borderWidth = 2
-    } catch {
-      print(error)
-      return
-    }
-    videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-    videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-    videoPreviewLayer?.frame = view.layer.bounds
-    view.layer.addSublayer(videoPreviewLayer!)
-
-    label = UILabel()
-    label.textColor = .white
-    label.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(label)
+    qrReader = QRReader(frame: .zero)
+    qrReader.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(qrReader)
     NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+      qrReader.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      qrReader.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      qrReader.widthAnchor.constraint(equalTo: view.widthAnchor),
+      qrReader.heightAnchor.constraint(equalTo: qrReader.widthAnchor)
       ])
+    qrReader.start()
 
-    if let qrCodeFrameView = qrCodeFrameView {
-      view.addSubview(qrCodeFrameView)
-    }
-    captureSession.startRunning()
+    captureReceiptButton = UIButton(type: .system)
+    captureReceiptButton.translatesAutoresizingMaskIntoConstraints = false
+    captureReceiptButton.setTitle("Capture Receipt", for: .normal)
+    view.addSubview(captureReceiptButton)
+    NSLayoutConstraint.activate([
+      captureReceiptButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      captureReceiptButton.topAnchor.constraint(equalTo: qrReader.bottomAnchor, constant: 10)
+      ])
   }
 }
-
-extension CameraVC: AVCaptureMetadataOutputObjectsDelegate {
-  func metadataOutput(_ captureOutput: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-
-    guard metadataObjects.count > 0, let metadataObj = metadataObjects[0] as? AVMetadataMachineReadableCodeObject, metadataObj.type == AVMetadataObject.ObjectType.qr else {
-      qrCodeFrameView.frame = CGRect.zero
-      label.backgroundColor = .clear
-      label.text = ""
-      return
-    }
-
-    let barCodeObject = videoPreviewLayer.transformedMetadataObject(for: metadataObj)
-    qrCodeFrameView.frame = barCodeObject!.bounds
-
-    label.backgroundColor = .black
-    label.text = metadataObj.stringValue
-  }
-}
-
---
+++ payj/payj/RoomVC.swift
@@ -0,0 +1,17 @@
+//
+//  RoomVC.swift
+//  payj
+//
+//  Created by Saurav Mitra on 03/03/2018.
+//  Copyright © 2018 PayJ. All rights reserved.
+//
+
+import UIKit
+
+class RoomVC: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    
+  }
+}

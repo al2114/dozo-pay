985c2f7f3da64a62730393d5e85706bc6ffb3c1b
Saurav Mitra <mitrasaurav97@gmail.com>
2018-03-13 21:11:30 +0000

    Remove random file

--- iOS/pesto/asdf.m
+++
@@ -1,9 +0,0 @@
-//
-//  asdf.m
-//  Pesto Pay
-//
-//  Created by Saurav Mitra on 09/03/2018.
-//  Copyright © 2018 Pesto Techonologies Ltd. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>

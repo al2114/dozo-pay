43b03d06c6d899c48dbdd2b9111834fb646b6f18
Andrew Li <al2114@ic.ac.uk>
2018-04-02 21:01:08 +0100

    Move login form script

--- backend/api-server/static/scripts/script.js
+++ backend/api-server/static/scripts/script.js
@@ -21,18 +21,12 @@ function getCookie(name) {
 }
 
 function eraseCookie(name) {
-    console.log("Erasing cookie: " + name)
     document.cookie = name+'=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
 }
 
 function logout() {
     eraseCookie("credentials");
-    window.location.reload(false); 
-}
-
-function login() {
-    setCookie("user_id",18,1);
-    window.location.reload(false); 
+    window.location.reload(false);
 }
 
 function getURLParameter(name) {
@@ -49,31 +43,3 @@ function makeHttpObject() {
 
   throw new Error("Could not create HTTP request object.");
 }
-
-$("form").submit(function() { 
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    let loginRequest = {
-        username: username,
-        password: password
-    };
-
-    var request = makeHttpObject();
-    request.open('POST', '/login', false);
-    request.setRequestHeader("Content-type", "application/json");
-    request.send(JSON.stringify(loginRequest));
-    let response = JSON.parse(request.responseText)
-    if(response['successful']){
-        let redirect = getURLParameter("redirect");
-        if(redirect != null){
-            document.location.href=redirect;
-        }
-        else {
-            document.location.href="/";
-        }
-    }
-    else {
-        $(".hidden").removeClass("hidden");
-    }
-    return false;
-});
